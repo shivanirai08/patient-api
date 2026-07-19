15f5a9941cbaaed12bc3378b794ad3b166df92c9
shivanirai08 <raishivani1406@gmail.com>
2026-07-20 00:24:43 +0530

    Add patient retrieval endpoints to FastAPI application
    - Implemented a function to load patient data from a JSON file.
    - Added endpoint to retrieve all patients and another to get a patient by ID.
    - Included error handling for non-existent patient IDs.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import json
 
 app = FastAPI()
 
@@ -6,5 +7,22 @@ app = FastAPI()
 def home():
     return {"message": "Hello, This is patient management system"}
 
+
+def load_patients():
+    with open("patients.json", "r") as file:
+        return json.load(file);
+
+
+@app.get("/patients")
+def get_all():
+    return load_patients()
+
+
+@app.get("/patients/{patient_id}")
+def get_by_id(patient_id : str):
+    if patient_id in load_patients():
+        return load_patients()[patient_id]
+    else:
+        raise HTTPException(status_code = 404, detail = "Patient not found")
 
 
